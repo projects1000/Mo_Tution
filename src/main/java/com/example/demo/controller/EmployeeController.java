947a828d16020eb4881f8d92d4b1b0787882f5c3
unknown <githubprojects100@gmail.com>
2023-03-22 13:26:28 +0530

    modified code

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import java.util.*;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -9,15 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.example.demo.entity.Employee;
-
 import com.example.demo.service.EmployeeServiceImpl;
-
-import jakarta.websocket.server.PathParam;
 
 @RestController
 
