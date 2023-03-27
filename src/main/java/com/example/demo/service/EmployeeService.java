7a4a07e714f7249b489a65c5fc301d077481a032
bijayplates <99069889+bijayplates@users.noreply.github.com>
2023-03-28 01:10:46 +0530

    Emp services modified

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -7,12 +7,12 @@ import com.example.demo.entity.Employee;
 public interface EmployeeService 
 {
 	
-	Employee saveEmployee(Employee employee);
+	public Employee saveEmployee(Employee employee);
 
 	public List<Employee> getAllEmployee();
 
-	Employee updateById(long id, Employee employee);
+	public Employee updateById(long id, Employee employee);
 
-	void deleteById(long id);
+	public void deleteById(long id);
 
 }
